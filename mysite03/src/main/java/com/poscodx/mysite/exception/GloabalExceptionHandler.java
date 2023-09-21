9a8cf6b39d4ebb5836d09483f9e4957dda3fbdbb
s2lilac <s2cherryy@gmail.com>
2023-09-21 12:16:10 +0900

    mysite03 logback #finish

--- mysite03/src/main/java/com/poscodx/mysite/exception/GloabalExceptionHandler.java
+++ mysite03/src/main/java/com/poscodx/mysite/exception/GloabalExceptionHandler.java
@@ -1,5 +1,7 @@
 package com.poscodx.mysite.exception;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -10,13 +12,16 @@ import java.io.StringWriter;
 @ControllerAdvice
 public class GloabalExceptionHandler {
 
+    private static final Log logger = LogFactory.getLog(GloabalExceptionHandler.class);
+
+
     @ExceptionHandler(Exception.class)
     public String handlerException(Model model, Exception e) {
         //1. Logging
         StringWriter errors = new StringWriter();
         e.printStackTrace(new PrintWriter(errors));
         System.out.println(errors.toString());
-
+        logger.error(errors.toString());
 
 //        System.out.println(e);
         e.printStackTrace();
